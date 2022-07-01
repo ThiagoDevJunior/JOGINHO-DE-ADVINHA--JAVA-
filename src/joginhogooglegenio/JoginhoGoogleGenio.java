5f69c36cafe48a738e48bea5e5bcb676318d801c
Thiago Angelo <100737987+ThiagoDevJunior@users.noreply.github.com>
2022-06-30 22:49:32 -0300

    Add files via upload

---
+++ src/joginhogooglegenio/JoginhoGoogleGenio.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package joginhogooglegenio;
+
+/**
+ *
+ * @author TIAGO
+ */
+public class JoginhoGoogleGenio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Tela mostrar = new Tela();
+        
+        mostrar.setVisible(true);
+
+    }
+    
+}
